fdd28a357eaf912d5eb9f6e5b1c74be20435f40f
DarlynZen <202014037@uns.edu.pe>
2026-06-04 18:21:36 -0500

    Implement routing and base component: add navigation links, configure routes, and create base component with template and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,12 @@ import { Home } from './home/home';
 import { Economics } from './economics/economics';
 import { Politics } from './politics/politics';
 import { Sports } from './sports/sports';
+import { Base } from './base/base';
 
 export const routes: Routes = [
-    {
-        path: "home",
-        component: Home
-    },
-    {
-        path: "economics",
-        component: Economics
-    },
-    {
-        path: "politics",
-        component: Politics
-    },
-    {
-        path: "sports",
-        component: Sports
-    }
+    {path: "", component: Base},
+    {path: "home", component: Home},
+    {path: "economics", component: Economics},
+    {path: "politics", component: Politics},
+    {path: "sports", component: Sports}
 ];
